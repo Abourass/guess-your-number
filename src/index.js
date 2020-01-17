84db6cca01d8105ccf112fd36d50f7d606376eff
Abourass <abourassa@assetval.com>
2020-01-17 13:01:37 -0700

    Make it so number one

---
+++ src/index.js
@@ -0,0 +1,72 @@
+const inquirer = require('inquirer');
+
+const guessYourNumber = async() => {
+  const config = {
+    low: 0,
+    high: 10000,
+    guesses: []
+  };
+
+  const chooseANumber = async() => {
+    inquirer.prompt([{type: 'number', name: 'number', message: 'Choose a (whole) number between 1 - 9999'}]).then((answers) => {
+      config.number = answers.number;
+      console.log('Number to guess is: ', config.number);
+      makeGuess();
+    })
+  };
+
+  const checkGuess = (guess) => {
+    let response;
+    if (guess > config.number){
+      response = 'Lower';
+      config.guesses.push({guess: guess, response: response});
+    }
+
+    if (guess < config.number){
+      response = 'Higher';
+      config.guesses.push({guess: guess, response: response});
+    }
+
+    if (guess === config.number){
+      response = 'You Win!';
+      config.guesses.push({guess: guess, response: response});
+    }
+    return makeGuess(response);
+  };
+
+  const makeGuess = (response = null) => {
+    const middleNumber = Math.floor(config.high / 2);
+    if (config.guesses.length > 30){
+      console.log('Could not guess in 30 guesses');
+      return console.table(config.guesses);
+    }
+    if (response == null){ checkGuess(middleNumber) } // First Guess
+    if (response === 'You Win!'){
+      console.table(config.guesses);
+      return console.log(`Winning Guess in ${config.guesses.length} guesses`)
+    }
+    if (response === 'Lower'){ checkGuess(generateNumber(response))}
+    if (response === 'Higher'){ checkGuess(generateNumber(response))}
+  };
+
+  const generateNumber = (response) => {
+    let newGuess;
+    while (typeof newGuess !== 'number'){
+      newGuess = checkNumberGen(Math.floor(Math.random() * config.high), response);
+    }
+    return newGuess
+  };
+
+  const checkNumberGen = (newGuess) => {
+    for (let i = 0; i < config.guesses.length; i++){
+      const guessHistory = config.guesses[i];
+      if (guessHistory.response === 'Lower' && newGuess > guessHistory.guess){ return 'failed'; }
+      if (guessHistory.response === 'Higher' && newGuess < guessHistory.guess){ return 'failed'; }
+      if (guessHistory.guess === newGuess){ return 'failed'; }
+    }
+    return newGuess
+  };
+
+  await chooseANumber();
+};
+guessYourNumber();
